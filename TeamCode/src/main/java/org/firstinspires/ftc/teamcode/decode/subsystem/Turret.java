8eaae2708ca0f48a9fa6dc151b8aa25e6f25787d
TheRealRamo1234 <omarnebula14@gmail.com>
2025-10-29 01:35:27 -0400

    Gulp Fixed static heading conversion methods
    - Gulp
    
    - Gulp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Turret.java
@@ -162,15 +162,14 @@ public class Turret extends Subsystem<Turret.TurretStates> {
     }
 
     public static double turretHeadingToStandardHeading(double turretHeadingDeg) {
-        double standardDeg = (90.0 - turretHeadingDeg + 360.0) % 360.0;
-        return Math.toDegrees(standardDeg);
+        return ((90.0 - turretHeadingDeg + 360.0) % 360.0);
     }
 
     /** Optional reverse conversion:
      *  math radians (0 = East, CCW+) → turret-domain degrees (0 = North, 90° = East, CW+)
      */
     public static double standardHeadingRadToTurretHeading(double standardHeadingDeg) {
-        double turretDeg = 90.0 - Math.toDegrees(standardHeadingDeg);
+        double turretDeg = 90.0 - (standardHeadingDeg);
         turretDeg = (turretDeg % 360.0 + 360.0) % 360.0;
         return turretDeg;
     }
