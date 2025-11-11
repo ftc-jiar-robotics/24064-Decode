7ba7fad939de03e048634f4b9d1beceda8d0e968
Arshad Anas <arshadanas900+github@gmail.com>
2025-11-11 15:44:45 -0500

    Motifs.getScoringInstructions()
    calculate which slot in the spindexer to start with, and which direction to run the spinnydexer
    also how many artifacts after the first one to shoot
    returns null if motif not possible rn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Motifs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Motifs.java
@@ -10,6 +10,10 @@ import com.bylazar.configurables.annotations.Configurable;
 import org.firstinspires.ftc.teamcode.decode.control.gainmatrix.HSV;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Configurable
 public class Motifs {
 
@@ -148,14 +152,43 @@ public class Motifs {
         return index;
     }
 
-    public static int[] reverseAt(int center, int[] array) {
-        assert array.length == 3;
-        int[] ints = array.clone();
-        int j = (center + 1) % 3;
-        int k = (j + 1) % 3;
-        int valJ = ints[j];
-        ints[j] = ints[k];
-        ints[k] = valJ;
-        return ints;
+    public static MotifScoringInstructions getScoringInstructions(Motif effectiveMotif, Artifact[] spindexerSlots) {
+
+        // indices of spindexer slots
+        List<Integer> slotIndices = Arrays.asList(0, 1, 2);
+
+        // the different colors in the provided motif
+        Artifact
+                m0 = effectiveMotif.artifacts[0],
+                m1 = effectiveMotif.artifacts[1],
+                m2 = effectiveMotif.artifacts[2];
+
+        // find index of first artifact color needed for motif
+        int firstArtifactIndex = indexOf(m0, spindexerSlots);
+
+        // we don't have the first color needed, continue intaking
+        if (firstArtifactIndex == -1) return null;
+
+        // rotate indices so the index of the first color needed is in the first slot
+        Collections.rotate(slotIndices, -firstArtifactIndex);
+
+        // if the next artifact (clockwise from the first one) is NOT the right motif color,
+        // and the third IS the right color, run the spindexer the other way
+        boolean counterClockwise = spindexerSlots[slotIndices.get(1)] != m1 && spindexerSlots[slotIndices.get(2)] == m1;
+        if (counterClockwise)
+            Collections.swap(slotIndices, 1, 2);
+
+        // check if we have the right second and third artifacts to complete the motif
+        // if we reversed the spindexer earlier, we know we have the second artifact needed, so
+        // skip that check with a short circuit OR ||
+        int additionalArtifacts = 0;
+        if (counterClockwise || spindexerSlots[slotIndices.get(1)] == m1) additionalArtifacts++;
+        if (spindexerSlots[slotIndices.get(2)] == m2) additionalArtifacts++;
+
+        return new MotifScoringInstructions(
+                counterClockwise,
+                firstArtifactIndex,
+                additionalArtifacts
+        );
     }
 }
