1881c056b335cf2bafc94d5e5f6a487e862c94b7
Arshad Anas <arshadanas900+github@gmail.com>
2025-11-12 16:20:04 -0500

    allowOneWrong parameter
    motif: GPP, spindexer: PPP will still score

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Motif.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Motif.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.decode.subsystem;
 
+import static org.firstinspires.ftc.teamcode.decode.subsystem.Artifact.EMPTY;
 import static org.firstinspires.ftc.teamcode.decode.subsystem.Artifact.GREEN;
 import static org.firstinspires.ftc.teamcode.decode.subsystem.Artifact.PURPLE;
 
@@ -47,18 +48,39 @@ public enum Motif {
 
     /**
      * @param spindexerSlots Artifacts available in the spindexer (0 = front, 1 = back left, 2 = back right)
+     * @param allowOneWrong Allow one wrong artifact color when scoring three artifacts
      * @return The order to score the artifacts in the spindexer
      */
-    public int[] getScoringOrder(Artifact... spindexerSlots) {
-        assert spindexerSlots.length == 3;
+    public int[] getScoringOrder(boolean allowOneWrong, Artifact... spindexerSlots) {
+        int length = spindexerSlots.length;
+        assert length == 3;
+
+        allowOneWrong = allowOneWrong && EMPTY.countOccurrencesIn(spindexerSlots) == 0;
 
         int firstArtifactIndex = first.firstOccurrenceIn(spindexerSlots);
 
-        if (firstArtifactIndex == -1)
-            return new int[]{};
+        if (firstArtifactIndex == -1) {
+            if (!allowOneWrong)
+                return new int[]{};
 
-        int 
-                length = spindexerSlots.length,
+            int secondArtifactIndex = second.firstOccurrenceIn(spindexerSlots);
+
+            if (secondArtifactIndex == -1)
+                return new int[]{};
+
+            firstArtifactIndex = wrap(secondArtifactIndex - 1, length);
+            int thirdArtifactIndex = wrap(secondArtifactIndex + 1, length);
+
+            return
+                    spindexerSlots[thirdArtifactIndex] == third ?
+                            new int[]{firstArtifactIndex, secondArtifactIndex, thirdArtifactIndex} :
+                            spindexerSlots[firstArtifactIndex] == third ?
+                                    new int[]{thirdArtifactIndex, secondArtifactIndex, firstArtifactIndex} :
+                                    new int[]{}
+            ;
+        }
+
+        int
                 secondArtifactIndex = wrap(firstArtifactIndex + 1, length),
                 thirdArtifactIndex = wrap(firstArtifactIndex + 2, length);
 
@@ -70,15 +92,17 @@ public enum Motif {
 
         return 
                 spindexerSlots[secondArtifactIndex] != second ?
-                        new int[]{firstArtifactIndex} :
-                        spindexerSlots[thirdArtifactIndex] != third ?
+                        allowOneWrong ?
+                                new int[]{firstArtifactIndex, secondArtifactIndex, thirdArtifactIndex} :
+                                new int[]{firstArtifactIndex} :
+                        spindexerSlots[thirdArtifactIndex] != third && !allowOneWrong ?
                                 new int[]{firstArtifactIndex, secondArtifactIndex} :
                                 new int[]{firstArtifactIndex, secondArtifactIndex, thirdArtifactIndex}
         ;
     }
 
-    public String getScoringInstructions(Artifact... spindexerSlots) {
-        int[] scoringOrder = getScoringOrder(spindexerSlots);
+    public String getScoringInstructions(boolean allowOneWrong, Artifact... spindexerSlots) {
+        int[] scoringOrder = getScoringOrder(allowOneWrong, spindexerSlots);
         if (scoringOrder.length == 0)
             return "Continue intaking";
         StringBuilder s = new StringBuilder(String.format("Score slot%s ", scoringOrder.length > 1 ? "s" : ""));
@@ -102,7 +126,7 @@ public enum Motif {
                                 c2 = second.name().charAt(0),
                                 c3 = third.name().charAt(0);
                         System.out.println("Motif: " + motif + ", Spindexer: " + (c1 == 'E' ? '_' : c1) + (c2 == 'E' ? '_' : c2) + (c3 == 'E' ? '_' : c3) + " --> " +
-                                motif.getScoringInstructions(first, second, third));
+                                motif.getScoringInstructions(true, first, second, third));
                     }
     }
 }
