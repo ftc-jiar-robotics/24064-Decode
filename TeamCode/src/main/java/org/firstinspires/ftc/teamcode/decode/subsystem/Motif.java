11998a5deddc92110bde87b6e13496bdcb83afb4
Arshad Anas <79271473+arshadanas@users.noreply.github.com>
2025-11-12 12:52:03 -0500

    return int[] scoringOrder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Motif.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/Motif.java
@@ -42,41 +42,47 @@ public enum Motif {
     }
 
     /**
-     * @param firstArtifactIndex Spindexer slot index of the first artifact to score
      * @param spindexerSlots Artifacts available in the spindexer (0 = front, 1 = back left, 2 = back right)
-     * @return Run spindexer counter-clockwise (CCW) when scoring motif
+     * @return The order to score the artifacts in the spindexer
      */
-    public boolean scoreCounterClockwise(int firstArtifactIndex, Artifact... spindexerSlots) {
-        int length = spindexerSlots.length;
-        Artifact secondArtifact = spindexerSlots[(firstArtifactIndex + 1) % length];
-        Artifact thirdArtifact = spindexerSlots[(firstArtifactIndex + 2) % length];
+    public int[] getScoringOrder(Artifact... spindexerSlots) {
 
-        // if the next artifact (clockwise from the first one) is NOT the  next motif color,
-        // and the third IS the next motif color, run the spindexer the other way
-        return secondArtifact != second && thirdArtifact == second;
-    }
+        int firstArtifactIndex = first.firstOccurrenceIn(spindexerSlots);
 
-    /**
-     * @param scoringCounterClockwise Running spindexer counter-clockwise (CCW) when scoring motif
-     * @param firstArtifactIndex Spindexer slot index of the first artifact to score
-     * @param spindexerSlots Artifacts available in the spindexer (0 = front, 1 = back left, 2 = back right)
-     * @return How many more artifacts to score after the first one (Can be 0, 1, or 2)
-     */
-    public int numAdditionalArtifacts(int firstArtifactIndex, Artifact... spindexerSlots) {
-        int length = spindexerSlots.length;
-        Artifact secondArtifact = spindexerSlots[(firstArtifactIndex + 1) % length];
-        Artifact thirdArtifact = spindexerSlots[(firstArtifactIndex + 2) % length];
-        boolean scoringCounterClockwise = scoreCounterClockwise(firstArtifactIndex, spindexerSlots);
+        if (firstArtifactIndex == -1)
+            return null;
 
-        return scoringCounterClockwise || secondArtifact == second ?
-            (scoringCounterClockwise ? secondArtifact : thirdArtifact) == third ? 2 : 1 : 0;
+        int 
+                length = spindexerSlots.length,
+                secondArtifactIndex = (firstArtifactIndex + 1) % length,
+                thirdArtifactIndex = (firstArtifactIndex + 2) % length;
+
+        if (spindexerSlots[secondArtifactIndex] != second && spindexerSlots[thirdArtifactIndex] == second) {
+            int temp = secondArtifactIndex;
+            secondArtifactIndex = thirdArtifactIndex;
+            thirdArtifactIndex = temp;
+        }
+
+        return 
+                spindexerSlots[secondArtifactIndex] != second ?
+                        new int[]{firstArtifactIndex} :
+                        spindexerSlots[thirdArtifactIndex] != third ?
+                                new int[]{firstArtifactIndex, secondArtifactIndex} :
+                                new int[]{firstArtifactIndex, secondArtifactIndex, thirdArtifactIndex}
+        ;
     }
 
     public String scoringInstructions(int firstArtifactIndex, Artifact... spindexerSlots) {
-        if (firstArtifactIndex == -1) return "Continue intaking";
-        boolean counterClockwise = scoreCounterClockwise(firstArtifactIndex, spindexerSlots);
-        int additionalArtifacts = numAdditionalArtifacts(firstArtifactIndex, spindexerSlots);
-        return "Score slot " + firstArtifactIndex + " artifact" + (additionalArtifacts == 0 ? "" : " first, then score " + additionalArtifacts + " more artifact" + (additionalArtifacts == 1 ? "" : "s") + " by rotating " + (counterClockwise ? "CCW" : "CW"));
+        int[] scoringOrder = getScoringOrder(spindexerSlots);
+        if (scoringOrder == null) 
+            return "Continue intaking";
+        String s = "Score ";
+        for (int i : scoringOrder)
+            s += i;
+        s += " = "
+        for (int i : scoringOrder)
+            s += spindexerSlots[i];
+        return s;
     }
 
     public static void main(String[] args) {
@@ -90,9 +96,8 @@ public enum Motif {
                                 c1 = first.name().charAt(0),
                                 c2 = second.name().charAt(0),
                                 c3 = third.name().charAt(0);
-                        int firstArtifactIndex = motif.first.firstOccurrenceIn(first, second, third);
                         System.out.println("Motif: " + motif + ", Spindexer: " + (c1 == 'E' ? '_' : c1) + (c2 == 'E' ? '_' : c2) + (c3 == 'E' ? '_' : c3) + " --> " +
-                                motif.scoringInstructions(firstArtifactIndex, first, second, third));
+                                motif.scoringInstructions(first, second, third));
                     }
     }
 }
