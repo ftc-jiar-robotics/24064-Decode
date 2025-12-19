c6f7f68b51ed1a468a99a62704e04f3960312197
marvelsatjiar <cfaunlol@gmail.com>
2025-12-19 17:23:27 -0500

    #54 Add cursor and selection for auto config

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/opmodes/auto/ConfigAuto.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.decode.opmodes.auto;
+
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.A;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.B;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.DPAD_DOWN;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.DPAD_LEFT;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.DPAD_RIGHT;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.DPAD_UP;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.LEFT_BUMPER;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.RIGHT_BUMPER;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.RIGHT_STICK_BUTTON;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.X;
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.Y;
+import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.dashTelemetry;
+import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.isSlowMode;
+import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.robot;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.pedropathing.geometry.Pose;
+
+import org.firstinspires.ftc.teamcode.decode.subsystem.AutoConfig;
+import org.firstinspires.ftc.teamcode.decode.subsystem.Common;
+
+public class ConfigAuto extends AbstractAuto {
+    protected AutoConfig autoConfig;
+
+    @Override
+    protected Pose getStartPose() {
+        return null;
+    }
+
+    @Override
+    protected void configure() {
+        autoConfig = new AutoConfig();
+
+        GamepadEx gamepadEx1 = new GamepadEx(gamepad1);
+        Common.dashTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        int screenNumber = 0;
+
+        while (opModeInInit() && !(gamepadEx1.isDown(LEFT_BUMPER) && gamepadEx1.isDown(RIGHT_BUMPER))) {
+            gamepadEx1.readButtons();
+
+            switch (screenNumber) {
+                case 0:
+                    autoConfig.doConfig(autoConfig.allianceChoices, gamepadEx1.wasJustPressed(DPAD_UP), gamepadEx1.wasJustPressed(DPAD_DOWN), screenNumber);
+                    break;
+                case 1:
+                    autoConfig.doConfig(autoConfig.sideChoices, gamepadEx1.wasJustPressed(DPAD_UP), gamepadEx1.wasJustPressed(DPAD_DOWN), screenNumber);
+                    break;
+                default:
+                    autoConfig.doConfig(autoConfig.pathChoices, gamepadEx1.wasJustPressed(DPAD_UP), gamepadEx1.wasJustPressed(DPAD_DOWN), screenNumber);
+                    break;
+            }
+
+            if (gamepadEx1.wasJustPressed(DPAD_RIGHT)) screenNumber++;
+            if (gamepadEx1.wasJustPressed(DPAD_LEFT)) screenNumber--;
+        }
+
+        autoConfig.reduceRequireList(screenNumber);
+    }
+
+    @Override
+    protected void onRun() {
+
+    }
+}
