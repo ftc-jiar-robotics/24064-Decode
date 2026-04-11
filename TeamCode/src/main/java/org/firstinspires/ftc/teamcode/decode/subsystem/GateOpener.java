b9dd10f255c225afc0a67d822019969b955f6b11
Kayra Ucuncu <kayraucuncu2008@gmail.com>
2026-04-10 21:45:10 -0400

    automatic gate open
    Refactor GateOpener to use an enum (OPEN, CLOSE, AUTOMATIC) instead of a boolean and add automatic open/close logic based on robot pose and heading. Introduces zone/angle constants (GATE_OPEN_*), inGateZone() detection, updateOpener() helper, and telemetry for state and zone info. Also updates Subsystem generic type and imports. Adjust RobotActions to call gateOpener.set(...) with the new GateOpener.GateOpenerStates values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/GateOpener.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/subsystem/GateOpener.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.decode.subsystem;
 
 import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.NAME_GATE_OPENER_RED_SERVO;
 import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.NAME_GATE_OPENER_BLUE_SERVO;
+import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.robot;
 import static org.firstinspires.ftc.teamcode.decode.subsystem.Common.telemetry;
 
 import com.bylazar.configurables.annotations.Configurable;
@@ -11,10 +12,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.decode.util.CachedServo;
 
 @Configurable
-public class GateOpener extends Subsystem<Boolean> {
+public class GateOpener extends Subsystem<GateOpener.GateOpenerStates> {
     private final CachedServo gateRed;
     private final CachedServo gateBlue;
-    private boolean isOpen = false;
+
+    public enum GateOpenerStates {
+        OPEN, CLOSE, AUTOMATIC
+    }
+
+    private GateOpenerStates currentState = GateOpenerStates.CLOSE;
 
     public static double
             CLOSED_ANGLE_RED = 50,
@@ -22,7 +28,19 @@ public class GateOpener extends Subsystem<Boolean> {
             BLOCKING_ANGLE_RED = 175,
             CLOSED_ANGLE_BLUE = 230,
             OPEN_ANGLE_BLUE = 105,
-            BLOCKING_ANGLE_BLUE = 105;
+            BLOCKING_ANGLE_BLUE = 105,
+            GATE_OPEN_Y_MIN = 59.4,
+            GATE_OPEN_Y_MAX = 77.9,
+            GATE_OPEN_X_MIN_BLUE = 1.8,
+            GATE_OPEN_X_MAX_BLUE = 19.5,
+            GATE_OPEN_X_MIN_RED = 144-GATE_OPEN_X_MIN_BLUE,
+            GATE_OPEN_X_MAX_RED = 144-GATE_OPEN_X_MAX_BLUE,
+            GATE_OPEN_ANGLE_MIN_BLUE = 90,
+            GATE_OPEN_ANGLE_MAX_BLUE = 180,
+            GATE_OPEN_ANGLE_MIN_RED = 0,
+            GATE_OPEN_ANGLE_MAX_RED = 90;
+
+    private boolean isOpen = false;
 
     public GateOpener(HardwareMap hw) {
         gateRed  = new CachedServo(hw, NAME_GATE_OPENER_RED_SERVO, Common.SERVO_25_KG_MIN, Common.SERVO_25_KG_MAX, AngleUnit.DEGREES);
@@ -30,29 +48,62 @@ public class GateOpener extends Subsystem<Boolean> {
     }
 
     @Override
-    protected void set(Boolean open) {
-        isOpen = open;
+    public void set(GateOpenerStates state) {
+        currentState = state;
     }
 
     @Override
-    public Boolean get() {
-        return isOpen;
+    public GateOpenerStates get() {
+        return currentState;
+    }
+
+    private boolean inGateZone() {
+        double x = robot.drivetrain.getPose().getX();
+        double y = robot.drivetrain.getPose().getY();
+        double heading = Math.toDegrees(robot.drivetrain.getHeading());
+        double xMin = Common.isRed ? GATE_OPEN_X_MIN_RED : GATE_OPEN_X_MIN_BLUE;
+        double xMax = Common.isRed ? GATE_OPEN_X_MAX_RED : GATE_OPEN_X_MAX_BLUE;
+        double degMin = Common.isRed ? GATE_OPEN_ANGLE_MIN_RED : GATE_OPEN_ANGLE_MIN_BLUE;
+        double degMax = Common.isRed ? GATE_OPEN_ANGLE_MAX_RED : GATE_OPEN_ANGLE_MAX_BLUE;
+
+        boolean inRange = x >= xMin && x <= xMax && y >= GATE_OPEN_Y_MIN && y <= GATE_OPEN_Y_MAX;
+        boolean facingWall = heading >= degMin && heading <= degMax;
+
+        return inRange && facingWall;
+    }
+
+    private void updateOpener() {
+        if (Common.isRed) {
+            gateRed.turnToAngle(isOpen ? OPEN_ANGLE_RED : CLOSED_ANGLE_RED);
+            gateBlue.turnToAngle(CLOSED_ANGLE_BLUE);
+        } else {
+            gateBlue.turnToAngle(isOpen ? OPEN_ANGLE_BLUE : CLOSED_ANGLE_BLUE);
+            gateRed.turnToAngle(CLOSED_ANGLE_RED);
+        }
     }
 
     @Override
     public void run() {
-        if (Common.isRed) {
-            gateRed.turnToAngle(isOpen ? OPEN_ANGLE_RED : CLOSED_ANGLE_RED);
-            gateBlue.turnToAngle(/*isOpen ? BLOCKING_ANGLE_BLUE : */CLOSED_ANGLE_BLUE);
-        } else {
-            gateBlue.turnToAngle(isOpen ? OPEN_ANGLE_BLUE : CLOSED_ANGLE_BLUE);
-            gateRed.turnToAngle(/*isOpen ? BLOCKING_ANGLE_RED : */CLOSED_ANGLE_RED);
+        switch (currentState) {
+            case OPEN:
+                isOpen = true;
+                break;
+            case CLOSE:
+                isOpen = false;
+                break;
+            case AUTOMATIC:
+                isOpen = inGateZone();
+                break;
         }
+
+        updateOpener();
     }
 
     @Override
     public void printTelemetry() {
         telemetry.addLine("GATE OPENER");
+        telemetry.addData("state (enum): ", currentState);
         telemetry.addData("is open (BOOLEAN): ", isOpen);
+        telemetry.addData("in gate zone (BOOLEAN): ", inGateZone());
     }
 }
